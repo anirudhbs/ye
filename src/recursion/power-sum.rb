d96c6e2160daf84da8a05ac39e23988bb4065911
Anirudh <anirudhbs96@gmail.com>
2020-08-16 03:32:32 +0530

    Remove ruby solutions

--- src/recursion/power-sum.rb
+++
@@ -1,9 +0,0 @@
-# https://www.hackerrank.com/challenges/the-power-sum/
-def power_sum(number, power, acc = 1)
-  cut = acc ** power
-  return 0 if (cut > number)
-  return 1 if (cut == number)
-  power_sum(number, power, acc + 1) + power_sum(number - cut, power, acc + 1)
-end
-
-puts power_sum(100,3)
--- src/recursion/recursive-digit-sum.rb
+++
@@ -1,9 +0,0 @@
-# https://www.hackerrank.com/challenges/recursive-digit-sum/
-def superDigit(number, times)
-  number = number.to_s * times
-  return number if number.length == 1
-  sum = number.split('').inject(0) { |acc, cur| acc + cur.to_i }
-  superDigit(sum, 1)
-end
-
-puts superDigit(148, 3)
